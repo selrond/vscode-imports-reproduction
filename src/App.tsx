b619fb19777c99c38b344378e62219b8783a091b
Sebastian Andil <sebastian.andil@gmail.com>
2021-06-17 12:34:18 +0200

    Add basic reproduction

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>See App.tsx</h1>
+      {/* TODO: Try to write `<Comp` here and see if you get auto import suggestion */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Component = () => {
+  return (
+    <div>
+      Hello there!
+    </div>
+  )
+}
